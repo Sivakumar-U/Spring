9bcd4b2447a0a17e06876751e5f23e125bcc29d2
Sivakumar-U <usivakumarreddy@gmail.com>
2020-12-16 19:55:32 +0530

    Added: Code to create a project to learn spring concepts

---
+++ src/main/java/com/example/demo/Component/DemoBean.java
@@ -0,0 +1,12 @@
+package com.example.demo.Component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DemoBean {
+
+	public DemoBean() {
+
+	}
+
+}
--- src/main/java/com/example/demo/HelloWorldApplication.java
+++ src/main/java/com/example/demo/HelloWorldApplication.java
@@ -2,13 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.example.demo.Component.DemoBean;
 
 @SpringBootApplication
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		SpringApplication.run(HelloWorldApplication.class, args);
+		ApplicationContext context = SpringApplication.run(HelloWorldApplication.class, args);
+		DemoBean demoBean = context.getBean(DemoBean.class);
+		System.out.println("demoBean  " + demoBean.toString());
 	}
 
 }
