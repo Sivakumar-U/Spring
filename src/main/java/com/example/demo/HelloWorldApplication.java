50b5d1964123b9b2a5621c226206731ebcdb3051
Sivakumar-U <usivakumarreddy@gmail.com>
2020-12-16 20:15:14 +0530

    Added: Code to add logger to log messages

--- src/main/java/com/example/demo/HelloWorldApplication.java
+++ src/main/java/com/example/demo/HelloWorldApplication.java
@@ -1,19 +1,26 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 import com.example.demo.Component.DemoBean;
+import com.example.demo.Controller.HelloRestController;
 
 @SpringBootApplication
 public class HelloWorldApplication {
 
+	public static Logger logger = LoggerFactory.getLogger(HelloWorldApplication.class);
+
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		logger.debug("Hello world");
 		ApplicationContext context = SpringApplication.run(HelloWorldApplication.class, args);
 		DemoBean demoBean = context.getBean(DemoBean.class);
-		System.out.println("demoBean  " + demoBean.toString());
+		logger.debug("demoBean  " + demoBean.toString());
+		HelloRestController controller = context.getBean(HelloRestController.class);
+		logger.debug(controller.toString());
 	}
 
 }
